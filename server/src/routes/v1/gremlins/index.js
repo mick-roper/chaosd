a5861204194f0559841673509bd72de896654cd7
mick-roper <mick.roper@dazn.com>
2019-07-10 14:51:51 +0100

    tweaked the get route

--- server/src/routes/v1/gremlins/index.js
+++ server/src/routes/v1/gremlins/index.js
@@ -3,7 +3,7 @@ const { Router } = require('restify-router')
 module.exports.createGremlinRoutes = (register) => {
   const router = new Router()
   
-  router.get('/', async (_, res, next) => {
+  router.get('', async (_, res, next) => {
     res.status(200)
     res.json(register)
     next()
