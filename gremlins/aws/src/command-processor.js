5a97dd0af7d140652e70b1e914966ea6eb815a4b
mick-roper <mick.roper@dazn.com>
2019-07-10 13:49:46 +0100

    refactored teh codebase into a collection of handlers

--- gremlins/aws/src/command-processor.js
+++ gremlins/aws/src/command-processor.js
@@ -1,6 +1,14 @@
-module.exports.createCommandProcessor = (socket, logger) => command => {
+module.exports.createCommandProcessor = (socket, handlers, logger) => command => {
   if (!command) {
-    socket.emit('status', { type: 'info', message: 'received an empy command' })
+    socket.emit('status', { type: 'warn', message: 'received an empy command' })
+    return
+  }
+
+  const { type, payload } = command
+
+  if (!type || !payload) {
+    socket.emit('status', { type: 'warn', message: 'invalid command received', command })
+    return
   }
 
   logger.info(command)
