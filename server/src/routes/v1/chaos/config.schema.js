a4108f16fd1978b145147d33a5695d3fb551179f
mick-roper <mick.roper@dazn.com>
2019-07-10 10:28:09 +0100

    updated the schema

--- server/src/routes/v1/chaos/config.schema.js
+++ server/src/routes/v1/chaos/config.schema.js
@@ -1,22 +1,18 @@
 const joi = require('joi')
 
 const schema = joi.object().keys({
-  errors: {
-    chance: joi.number().min(0).max(1),
-    statusCode: joi.number().min(0).max(600)
+  error: {
+    chance: joi.number().min(0).max(1).required(),
+    statusCode: joi.number().min(0).max(600).required()
   },
   latency: {
-    chance: joi.number().min(0).max(1),
-    minMillis: joi.number().min(0).max(600000),
-    maxMillis: joi.number().min(0).max(600000),
+    chance: joi.number().min(0).max(1).required(),
+    minMillis: joi.number().min(0).max(600000).required(),
+    maxMillis: joi.number().min(0).max(600000).required(),
   }
 })
 
 module.exports.getValidationErrors = data => {
-  if (!data) {
-    return false
-  }
-
   const result = schema.validate(data)
 
   return result.error
