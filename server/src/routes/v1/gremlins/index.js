6a1838b6899be1ef83c1bd959060f208624577c1
mick-roper <mick.roper@dazn.com>
2019-07-10 14:43:02 +0100

    added some routes that expose gremlins

--- server/src/routes/v1/gremlins/index.js
+++ server/src/routes/v1/gremlins/index.js
@@ -3,7 +3,7 @@ const { Router } = require('restify-router')
 module.exports.createGremlinRoutes = (register) => {
   const router = new Router()
   
-  router.get('/gremlins', (_, res, next) => {
+  router.get('/', (_, res, next) => {
     res.status(200)
     res.json(register)
     next()
