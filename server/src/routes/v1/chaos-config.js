d6c7758b07833ef895b46f9c3f2770ccebceaf13
mick-roper <mick.roper@dazn.com>
2019-07-10 09:29:07 +0100

    added an entry point route

---
+++ server/src/routes/v1/chaos-config.js
@@ -0,0 +1,28 @@
+const { Router } = require('restify-router')
+
+const store = {}
+
+module.exports.createChaosConfigRouter = () => {
+  const router = new Router()
+
+  router.get('/:serviceId', async (req, res, next) => {
+    const { serviceId } = req.params
+    const { version = "$LATEST" } = req.query
+
+    const serviceConfigs = store[serviceId] | []
+    const config = serviceConfigs[version]
+
+    if (!config) {
+      res.status(400)
+      res.json({ message: `config ${version} does not exist` })
+      return next()
+    }
+
+    res.status(200)
+    res.json(config)
+
+    return next()
+  })
+  
+  return router
+}
