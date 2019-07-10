66c3c1df765a5e93df48f475bdcaf35ac36e32dc
mick-roper <mick.roper@dazn.com>
2019-07-10 11:06:24 +0100

    added logging middleware

--- server/src/middleware/logging.js
+++ server/src/middleware/logging.js
@@ -1,9 +1,26 @@
-module.exports.createLoggingMiddleware = () => (req, res, next) => {
-  const { headers } = req
-  const requestId = headers['x-request-id']
-  const correlationId = headers['x-correlation-id'] || 'some-correlation-id'
+const generateRequestId = () => {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  const length = Math.floor(Math.random() * 25)
+  let str = ''
 
-  
+  for (let i = 0; i < length; i++) {
+    str += chars.charAt(Math.floor(Math.random() * chars.length))
+  }
+
+  return str
 }
 
-const createLogger = ({ requestId, correlationId }) => ({})
+module.exports.createLoggingMiddleware = (loggerFactory) => async (req, res, next) => {
+  const { headers } = req
+  const requestId = generateRequestId()
+  const correlationId = headers['x-correlation-id']
+
+  req.logger = loggerFactory({ requestId, correlationId })
+  res.header('x-request-id', requestId)
+
+  if (correlationId) {
+    res.header('x-correlation-id', correlationId)
+  }
+
+  await next()
+}
