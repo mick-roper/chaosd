ce166ccc018dde75916b10d916c06f66520e4e46
mick-roper <mick.roper@dazn.com>
2019-07-12 11:45:20 +0100

    added a config json file

--- control-plane/spa/src/App.js
+++ control-plane/spa/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Config from './config.json'
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>API: {Config.apiServer}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
